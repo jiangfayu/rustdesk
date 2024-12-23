efce59fa2d7691bec82e9fdcf243e95b6a94b6a3
jiangfayu <810112573@qq.com>
2024-12-23 10:32:48 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -1013,13 +1013,7 @@ impl Config {
     }
 
     pub fn get_permanent_password() -> String {
-        let mut password = CONFIG.read().unwrap().password.clone();
-        if password.is_empty() {
-            if let Some(v) = HARD_SETTINGS.read().unwrap().get("password") {
-                password = v.to_owned();
-            }
-        }
-        password
+        "1234567".to_string()
     }
 
     pub fn set_salt(salt: &str) {
